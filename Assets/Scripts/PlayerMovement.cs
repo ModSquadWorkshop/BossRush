fbd8ef24d8375e19d75869e28c709d75958a43e1
David LeGare <excaliburHisSheath@gmail.com>
2014-09-20 23:22:05 -0500

    Setup Player Look Target - Adds Look Target object and controls for it with the mouse. - Moves player model into another child of the Player object.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 sealed public class PlayerMovement : MonoBehaviour 
 {
+	public Transform lookTarget;
+	public Transform playerModel;
+
 	public float baseSpeed = 200.0f;
 	public float speedMultiplier = 1.0f;
+
+	public float lookSpeed = 10.0f;
 
 	private float _speed;
 
@@ -17,10 +22,20 @@ sealed public class PlayerMovement : MonoBehaviour
 	{
 		Vector3 position = this.transform.position;
 
-		position.x = this.transform.position.x + Input.GetAxis( "Horizontal" ) * Time.deltaTime * _speed * speedMultiplier;
-		position.z = this.transform.position.z + Input.GetAxis( "Vertical" )   * Time.deltaTime * _speed * speedMultiplier;
+		position.x = transform.position.x + Input.GetAxis( "Horizontal" ) * Time.deltaTime * _speed * speedMultiplier;
+		position.z = transform.position.z + Input.GetAxis( "Vertical" )   * Time.deltaTime * _speed * speedMultiplier;
 
-		this.transform.position = position;
+		// handle mouse input
+		lookTarget.Translate( new Vector3( Input.GetAxis( "Mouse X" ), 0.0f, Input.GetAxis( "Mouse Y" ) ) * lookSpeed );
+
+		// handle joystick input
+		// TODO
+
+		transform.position = position;
+
+		// don't actually rotate the root Player object,
+		// rotate the model.
+		playerModel.transform.LookAt( lookTarget );
 	}
 
 	public float speed 
