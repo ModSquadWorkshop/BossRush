bb0bc6107b82fa46806fbd41cad6e7c5f4ec85d5
David LeGare <excaliburHisSheath@gmail.com>
2014-09-22 19:56:07 -0500

    Fix For Bullet Collision closes #19 - Changes Projectile script to treat bullets as physics objects. - The solution for collision misses was ultimately to make the collider on the bullets longer (my thanks to Andre B. for that trick).

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,34 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour 
+public class Projectile : MonoBehaviour
 {
-	public float speed = 150.0f; 			// in world units
-	public float maxDistance = 500.0f;		// in world units
-	public float maxTime = 8.0f; 			// in seconds
+	public float speed = 150.0f;            // in world units
+	public float maxDistance = 500.0f;      // in world units
+	public float maxTime = 8.0f;            // in seconds
 
-	private float _distanceTraveled;
-	private float _elapsedTime; 
-	
-	void Update() 
+	private Vector3 _startPoint;
+	private float _maxDistanceSquared;
+	private float _elapsedTime;
+
+	void Start()
 	{
-		float deltaSpeed = Time.deltaTime * speed;
+		rigidbody.velocity = transform.forward * speed;
+		_startPoint = transform.position;
+		_maxDistanceSquared = maxDistance * maxDistance;
+		_elapsedTime = 0.0f;
+	}
 
-		// move the projectile forward according to its rotation
-		this.transform.Translate( Vector3.forward * deltaSpeed );
-
-		// update properties to check if the projectile should be destroyed
-		_distanceTraveled += deltaSpeed;
+	void Update()
+	{
 		_elapsedTime += Time.deltaTime;
 
 		// if the projectile has traveled farther than its max distance, it is destroyed
-		if ( _distanceTraveled > maxDistance ) 
+		if ( ( transform.position - _startPoint ).sqrMagnitude > _maxDistanceSquared )
 		{
 			Destroy( this.gameObject );
 		}
 
 		// if the projectile has lasted longer the its max life time, it is destroyed
-		if ( _elapsedTime > maxTime ) 
+		if ( _elapsedTime > maxTime )
 		{
 			Destroy( this.gameObject );
 		}
