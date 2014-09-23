873c9e132d0bca06201b2857d3396c6496738e6f
David LeGare <excaliburHisSheath@gmail.com>
2014-09-23 11:24:09 -0500

    Shell Casings work on #21 - Guns now eject shell casings when they fire. - Made start method for Weapons virtual so that child classes can call parent's Start() method (the unity equivalent of chaining constructors).

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,11 +4,32 @@ using System.Collections;
 public class Gun : Weapon
 {
 	public GameObject projectile;
+	public Transform casingAnchor;
+	public GameObject casingEmitter;
+
+	public override void Start()
+	{
+		// we still need to call Weapon.Start() to initialize the timer and whatnot.
+		base.Start();
+
+		// instantiate casing emitter
+		casingEmitter = Instantiate( casingEmitter ) as GameObject;
+		casingEmitter.transform.parent = casingAnchor;
+
+		// we need to reset the transform so that it adhere's to the anchor's transform,
+		// otherwise unity try's to preserve it's exising location in world space
+		// when you child an object to another object.
+		casingEmitter.transform.localPosition = Vector3.zero;
+		casingEmitter.transform.localRotation = Quaternion.Euler( 0.0f, 0.0f, 0.0f );
+	}
 
 	public override void PerformPrimaryAttack()
 	{
 		// instantiate and initialize a bullet
 		InitializeBullet( Instantiate( projectile ) as GameObject );
+
+		// create shell casing
+		casingEmitter.particleSystem.Emit( 1 );
 
 		// reset and start the gun cooldown
 		_cooldownTimer.Reset( true );
