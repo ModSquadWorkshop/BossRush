927235a5ca8ca67afb05c0d086ec5f058861152f
David LeGare <excaliburHisSheath@gmail.com>
2014-09-23 21:38:26 -0500

    HealthSystem Observers work on #11 - Allows objects to register with a HealthSystem to be notified on death. - Adds LevelManager class which listens for player and boss deaths. Will eventually restart the scene when one dies.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject boss;
+
+	void Start()
+	{
+		// register with health systems
+		player.GetComponent<HealthSystem>().RegisterDeathCallback( new HealthSystem.DeathCallback( this.PlayerDied ) );
+		boss.GetComponent<HealthSystem>().RegisterDeathCallback( new HealthSystem.DeathCallback( this.BossDied ) );
+	}
+
+	public void PlayerDied( HealthSystem playerHealth )
+	{
+		// TODO
+		//Debug.Log( "Player Died" );
+	}
+
+	public void BossDied( HealthSystem bossHealth )
+	{
+		// TODO
+		Debug.Log( "Boss Died" );
+	}
+}
