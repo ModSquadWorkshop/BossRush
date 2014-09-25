5f81dcb45d2152620c87ff9008e5929def529e13
Westin Grube <westin28@hotmail.com>
2014-09-25 12:08:50 -0500

    Perk System closes #12 - script implemented, 3 example prefabs created, PerkTime script on Player for timed buffs

---
+++ Assets/Scripts/PerkSystem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerkSystem : MonoBehaviour 
+{
+	//bools to determine what is being modified by a perk object
+	public bool immunity;
+
+	//ammount to change various stats by
+	public float speedMod;
+	public float fireRateMod; //make a negative value
+	public float maxHealthMod;
+	public float damageMod;
+
+	//time a perk lasts (if using a timer);
+	public float length;
+	
+	void OnCollisionEnter( Collision other )
+	{
+		//Debug.Log ( "HIT" );
+		if( other.gameObject.tag == "Player" )
+		{
+			other.gameObject.GetComponent<PlayerMovement>().speedMultiplier += speedMod;
+
+			other.gameObject.GetComponent<HealthSystem>().maxHealth += maxHealthMod; //heal to new max once picked up?
+
+			//only effect currently held gun right now
+			other.gameObject.GetComponent<WeaponSystem>().currentWeapon.GetComponent<DamageSystem>().damageMultiplier += damageMod;
+			other.gameObject.GetComponent<WeaponSystem>().currentWeapon.GetComponent<Gun>().cooldown += fireRateMod;
+
+			if( immunity )
+			{
+				other.gameObject.GetComponent<PerkTime>().perkLength = length;
+				other.gameObject.GetComponent<PerkTime>().BeginPerk();
+				other.gameObject.GetComponent<HealthSystem>().immune = true;
+			}
+			Destroy(this.gameObject);
+		}
+	}
+}
+/*
+Questions for designers
+_______________________
+Should a max health boost also heal the player?
+Should a fire rate/ammo/damage boost effect all guns held or just the one currently held? (GetComponentInChildren)
+*/
