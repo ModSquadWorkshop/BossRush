6970d887f30bcef0054b56e261cb94ade834d861
David LeGare <excaliburHisSheath@gmail.com>
2014-09-29 08:49:33 -0500

    Basic Audio for Guns work on #39 - Guns now play their audio source when they fire. As a consequence all guns now need an audio source.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -79,6 +79,7 @@ public class Gun : Weapon
 
 		// instantiate and initialize a bullet
 		InitializeBullet( Instantiate( projectile ) as GameObject );
+		audio.Play();
 
 		// update ammunition data
 		if ( !infiniteAmmo )
