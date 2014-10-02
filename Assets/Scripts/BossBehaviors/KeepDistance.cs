ee995191b52a727ae375ebc56533ff16043fc53a
David LeGare <excaliburHisSheath@gmail.com>
2014-10-01 20:21:43 -0500

    Keep Distance Movement - Adds a KeepDistance script which attempts to have the boss stay a set distance from its target.

---
+++ Assets/Scripts/BossBehaviors/KeepDistance.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepDistance : MonoBehaviour
+{
+	public Transform target;
+	public float distance;
+	public float moveSpeed;
+
+	private float _sqrDistance;
+	private Vector3 _movement;
+
+	void Start()
+	{
+		HealthSystem targetHealth = target.gameObject.GetComponent<HealthSystem>();
+		if ( targetHealth != null )
+		{
+			targetHealth.RegisterDeathCallback( new HealthSystem.DeathCallback( TargetDeath ) );
+		}
+
+		_sqrDistance = distance * distance;
+	}
+
+	void Update()
+	{
+		// move towards if too far, move away if too close
+		float currentSqrDistance = Vector3.SqrMagnitude( transform.position - target.position );
+		_movement = ( currentSqrDistance > _sqrDistance ) ? target.position - transform.position : transform.position - target.position;
+		_movement = Vector3.Normalize( _movement );
+	}
+
+	void FixedUpdate()
+	{
+
+		rigidbody.AddForce( _movement * moveSpeed * Time.fixedDeltaTime * rigidbody.mass * 150.0f );
+	}
+
+	public void TargetDeath( HealthSystem targetHealth )
+	{
+		Destroy( this );
+	}
+}
