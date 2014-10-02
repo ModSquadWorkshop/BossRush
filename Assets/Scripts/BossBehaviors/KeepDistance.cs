7abc778efd2d911ad2ebb9254a1ee6c09accbe4f
David LeGare <excaliburHisSheath@gmail.com>
2014-10-02 10:12:49 -0500

    Basic Physics Movement work on #47 - Adds a base PhysicsMovement script to give general controls to movement for physics-based objects. - Adds a Boss Physics material that removes friction to simplify physics movement.

--- Assets/Scripts/BossBehaviors/KeepDistance.cs
+++ Assets/Scripts/BossBehaviors/KeepDistance.cs
@@ -1,14 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class KeepDistance : MonoBehaviour
+public class KeepDistance : PhysicsMovement
 {
 	public Transform target;
 	public float distance;
-	public float moveSpeed;
-
-	private float _sqrDistance;
-	private Vector3 _movement;
 
 	void Start()
 	{
@@ -17,22 +13,14 @@ public class KeepDistance : MonoBehaviour
 		{
 			targetHealth.RegisterDeathCallback( new HealthSystem.DeathCallback( TargetDeath ) );
 		}
-
-		_sqrDistance = distance * distance;
 	}
 
 	void Update()
 	{
 		// move towards if too far, move away if too close
 		float currentSqrDistance = Vector3.SqrMagnitude( transform.position - target.position );
-		_movement = ( currentSqrDistance > _sqrDistance ) ? target.position - transform.position : transform.position - target.position;
+		_movement = ( currentSqrDistance > distance * distance ) ? target.position - transform.position : transform.position - target.position;
 		_movement = Vector3.Normalize( _movement );
-	}
-
-	void FixedUpdate()
-	{
-
-		rigidbody.AddForce( _movement * moveSpeed * Time.fixedDeltaTime * rigidbody.mass * 150.0f );
 	}
 
 	public void TargetDeath( HealthSystem targetHealth )
