8663ec8cd7922f638d993baa20b475e6eee02142
David LeGare <excaliburHisSheath@gmail.com>
2014-10-09 18:04:41 -0500

    Basic Boss Rush Attack work on #62 - Basic implementation of the boss rush attack. No damage yet, but it will attack at intervals and will switch between the turbo state and the basic state.

---
+++ Assets/Scripts/BossBehaviors/RushAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RushAttack : PhysicsMovement
+{
+	public delegate void RushEndCallback();
+
+	public float moveDistance;
+	public float attackDamage;
+
+	[HideInInspector] public Transform target;
+
+	private Vector3 _startPos;
+	private RushEndCallback _rushEndCallback;
+
+	public void Awake()
+	{
+		// ensure that _rushEndCallback isn't null
+		_rushEndCallback = delegate() { };
+	}
+
+	public void OnEnable()
+	{
+		_movement = Vector3.Normalize( target.position - transform.position );
+		_startPos = transform.position;
+	}
+
+	public void Update()
+	{
+		if ( ( _startPos - transform.position ).sqrMagnitude > moveDistance * moveDistance )
+		{
+			// disable self and notify listeners
+			enabled = false;
+			_rushEndCallback();
+			_rushEndCallback = delegate() { };
+		}
+	}
+
+	public void RegisterCallback( RushEndCallback callback )
+	{
+		_rushEndCallback += callback;
+	}
+}
---
+++ Assets/Scripts/BossBehaviors/SpiderTankStates/SpiderTankRushState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderTankRushState : SpiderTankState
+{
+	[HideInInspector] public SpiderTankState returnState;
+
+	private RushAttack _rushAttack;
+
+	public override void Awake()
+	{
+		base.Awake();
+
+		_rushAttack = GetComponent<RushAttack>();
+	}
+
+	public void OnEnable()
+	{
+		_rushAttack.target = spiderTank.player;
+		_rushAttack.RegisterCallback( new RushAttack.RushEndCallback( EndRush ) );
+		_rushAttack.enabled = true;
+	}
+
+	void EndRush()
+	{
+		enabled = false;
+		returnState.enabled = true;
+	}
+}
