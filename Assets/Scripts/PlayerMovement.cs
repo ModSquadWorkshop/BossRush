4c9dc4c96fc2e8911f2edc4acf2ca6312943dbfd
Zigurous <zigurous@gmail.com>
2014-10-27 18:49:10 -0500

    work for #50. Player dash now fully implemented. The player does not dash through objects anymore... 99.9% of the time. There's a very very rare chance that player can still dash through an object.. it has only happened to me once, but you have to be trying really really hard to make that happen. Setting the dash delay reduces the chance of it happening even more, so I really don't think it'll be a problem. The other thing to note is that the player and all of it's child objects should have their layer set to "Player". The dash will need to be tested more in the actual game scene as other gameplay features may or may not cause bugs with it.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,12 @@ sealed public class PlayerMovement : MonoBehaviour
 
 	private Timer _dashTimer;
 	private Timer _dashDelayTimer;
+	private Vector3 _dashOrigin;
 	private Vector3 _dashForward;
+	private RaycastHit _dashHit;
+	private float _dashMaxDistance;
+	private float _dashDistanceTraveled;
+	private int _dashLayerMask;
 
 	HealthSystem playerHealth;
 	CameraFollow camShake;
@@ -26,15 +31,20 @@ sealed public class PlayerMovement : MonoBehaviour
 
 	void Start()
 	{
-		//register for damage callback (rumble and shake)
+		// register for damage callback (rumble and shake)
 		playerHealth = this.GetComponent<HealthSystem>();
 		playerHealth.RegisterHealthCallback( TargetDamageCallback );
 
 		camShake = Camera.main.gameObject.GetComponent<CameraFollow>();
 		rumbler = Camera.main.gameObject.GetComponent<RumbleManager>();
 
+		// initialize dash timers
 		_dashTimer = new Timer( dashTime, 1 );
 		_dashDelayTimer = new Timer( dashDelay, 1 );
+
+		// create a ray casting layer mask that collides with everything accept "Player"
+		_dashLayerMask = 1 << LayerMask.NameToLayer( "Player" );
+		_dashLayerMask = ~_dashLayerMask; // invert the mask
 	}
 
 	void Update()
@@ -46,7 +56,7 @@ sealed public class PlayerMovement : MonoBehaviour
 
 			Vector3 forward = new Vector3( horizontalAxis, 0.0f, verticalAxis );
 
-			// cardinal movement
+			// cardinal movement (WASD)
 			rigidbody.velocity = forward * speed;
 
 			// handle mouse input
@@ -62,43 +72,71 @@ sealed public class PlayerMovement : MonoBehaviour
 				lookTarget.localPosition = gamePadLook;
 			}
 
+			// update the dash delay timer
+			// the player won't be able to dash again until this timer is done running
 			_dashDelayTimer.Update();
 
-			// handle dash
-			if ( Input.GetButtonDown( "Dash" ) )
+			// handle dash input
+			if ( Input.GetButtonDown( "Dash" ) ) 
 			{
-				if ( !_dashDelayTimer.running )
+				// insure the player isn't already dashing
+				if ( !_dashDelayTimer.running ) 
 				{
-					_dashTimer.Reset( true );
-					_dashForward = forward * dashDistance;
+					_dashOrigin = rigidbody.transform.position;
 
-					if ( stopWeaponInDash )
+					// calculate if the dash distance needs to be shorter according to any collisions that will happen
+					if ( Physics.Raycast( _dashOrigin, forward, out _dashHit, Mathf.Infinity, _dashLayerMask ) )
 					{
+						_dashMaxDistance = _dashHit.distance;
+					}
+					else
+					{
+						_dashMaxDistance = Mathf.Infinity;
+					}
+
+					if ( stopWeaponInDash ) 
+					{
+						// disable the player's weapon
 						WeaponSystem weapons = this.GetComponent<WeaponSystem>();
 						weapons.currentWeapon.enabled = false;
 					}
+
+					// start the dash
+					_dashDistanceTraveled = 0.0f;
+					_dashForward = forward * dashDistance;
+					_dashTimer.Reset( true );
 				}
 			}
 
-			// don't actually rotate the root Player object,
-			// rotate the model.
+			// don't actually rotate the root Player object, rotate the model
 			playerModel.transform.LookAt( lookTarget );
 		}
 		else
 		{
-			rigidbody.velocity = _dashForward * speed;
-
+			// update dash properties to determine if it needs to be stopped
+			_dashDistanceTraveled = Vector3.Distance( _dashOrigin, rigidbody.transform.position );
 			_dashTimer.Update();
 
-			if ( _dashTimer.complete )
+			if ( _dashTimer.complete ) 
 			{
-				if ( stopWeaponInDash )
-				{
-					WeaponSystem weapons = this.GetComponent<WeaponSystem>();
-					weapons.currentWeapon.enabled = true;
-				}
+				// a full dash occured
 
-				_dashDelayTimer.Reset( true );
+				StopDash();
+			}
+			else if ( _dashDistanceTraveled >= _dashMaxDistance )
+			{
+				// a partial dash occured because of collision
+
+				StopDash();
+
+				rigidbody.transform.position = _dashHit.point;
+				rigidbody.velocity = Vector3.zero;
+			}
+			else
+			{
+				// still currently dashing
+
+				rigidbody.velocity = _dashForward * speed;
 			}
 		}
 	}
@@ -109,6 +147,20 @@ sealed public class PlayerMovement : MonoBehaviour
 		{
 			return baseSpeed * speedMultiplier;
 		}
+	}
+
+	private void StopDash()
+	{
+		if ( stopWeaponInDash )
+		{
+			// enable the player's weapon
+			WeaponSystem weapons = this.GetComponent<WeaponSystem>();
+			weapons.currentWeapon.enabled = true;
+		}
+
+		// start the dash delay timer
+		// the player won't be able to dash again until this timer is complete
+		_dashDelayTimer.Reset( true );
 	}
 
 	void TargetDamageCallback( HealthSystem playerHealth, float damage )
