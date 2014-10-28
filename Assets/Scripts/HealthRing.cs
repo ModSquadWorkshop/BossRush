915054a37f1e52efa7e5b82f8426b8b5df4b7664
Westin Grube <westin28@hotmail.com>
2014-10-28 09:19:54 -0500

    #80 Health Indicator
    HealthRingTest
    -Only final half of ring is currently rendered, likely a problem with the texture the material uses
    -Once below half health, ring depletes as it should
    -Health bar increases upon health drop pickup as well

---
+++ Assets/Scripts/HealthRing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthRing : MonoBehaviour 
+{
+	HealthSystem playerHealth;
+	float maxHealth;
+
+	void Start () 
+	{
+		playerHealth = this.gameObject.GetComponentInParent<HealthSystem>();
+		maxHealth = this.gameObject.GetComponentInParent<HealthSystem>().maxHealth;
+	}
+
+	void Update () 
+	{
+		renderer.material.SetFloat( "_Cutoff", 1f - (playerHealth.health / maxHealth) );  //Mathf.InverseLerp( 0, Screen.width, Input.mousePosition.x ) ); 
+		//Debug.Log( Mathf.InverseLerp( 0, Screen.width, Input.mousePosition.x) );
+		//Debug.Log( 1f - (playerHealth.health / maxHealth) );
+	}
+}
+
