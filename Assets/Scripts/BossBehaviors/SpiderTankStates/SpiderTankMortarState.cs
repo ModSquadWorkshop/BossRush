9d5200a5b4a064ef245fb1390b530737c8cf402b
Adam Graham <zigurous@gmail.com>
2014-11-02 16:44:36 -0600

    #49. Improved the code of the SpiderTankMortarState by using an invoke instead of a delay timer.

--- Assets/Scripts/BossBehaviors/SpiderTankStates/SpiderTankMortarState.cs
+++ Assets/Scripts/BossBehaviors/SpiderTankStates/SpiderTankMortarState.cs
@@ -8,36 +8,23 @@ public class SpiderTankMortarState : SpiderTankState
 	public float delayBeforeLaunch;
 
 	private MortarAttack _mortarAttack;
-	private Timer _mortarDelayTimer;
 
 	public override void Awake()
 	{
 		base.Awake();
-
-		_mortarDelayTimer = new Timer( delayBeforeLaunch, 1 );
 
 		_mortarAttack = mortarLauncher.GetComponent<MortarAttack>();
 		if ( _mortarAttack != null )
 		{
 			// auto assign the target of the mortar attack to be the player
 			_mortarAttack.target = player.gameObject;
-
-			// start the delay timer
-			// when the delay is complete, the mortars start launching
-			_mortarDelayTimer.Start();
 		}
+
+		Invoke( "Launch", delayBeforeLaunch );
 	}
 
-	void Update()
+	void Launch()
 	{
-		if ( _mortarDelayTimer.running )
-		{
-			_mortarDelayTimer.Update();
-
-			if ( _mortarDelayTimer.complete )
-			{
-				_mortarAttack.Launch( amountOfMortars );
-			}
-		}
+		_mortarAttack.Launch( amountOfMortars );
 	}
 }
