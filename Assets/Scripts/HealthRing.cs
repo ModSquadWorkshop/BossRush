641b779a5e8feac61b3d04b8a90384a79b66739c
David LeGare <excaliburHisSheath@gmail.com>
2014-11-02 15:36:44 -0600

    Health Indicator Rotation work on #80 - Fixes the issue of the spider tank's health indicator rotation with the boss.

--- Assets/Scripts/HealthRing.cs
+++ Assets/Scripts/HealthRing.cs
@@ -2,11 +2,27 @@
 
 public class HealthRing : MonoBehaviour
 {
+	static Quaternion ROTATION = Quaternion.Euler( 90.0f, 0.0f, 0.0f );
+
+	private new Transform transform;
+
+	void Awake()
+	{
+		transform = GetComponent<Transform>();
+	}
+
 	void Start()
 	{
 		HealthSystem playerHealth = GetComponentInParent<HealthSystem>();
 		playerHealth.RegisterHealthCallback( UpdateHealthBar );
 		UpdateHealthBar( playerHealth, 0.0f ); // set initial health bar state
+	}
+
+	void Update()
+	{
+		// this allows the health ring to be childed to the object,
+		// but to not rotate as the object rotates.
+		transform.rotation = ROTATION;
 	}
 
 	public void UpdateHealthBar( HealthSystem playerHealth, float healthChange )
