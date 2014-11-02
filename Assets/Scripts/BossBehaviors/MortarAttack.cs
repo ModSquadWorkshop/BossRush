c41b45ffff8c40b250211eb75dc72d10292dfbfc
Adam Graham <zigurous@gmail.com>
2014-11-02 14:51:18 -0600

    #49. MortarAttacks now have the option to use predefined targets. If using predefined targets, a mortar picks a random target from the array. If we would like it to be sequential, I can change it (let me know). Using this option still allows for dynamic moving targets too, so we can have a mix of static target positions and moving target positions (such as the player). Making a pull request now.

--- Assets/Scripts/BossBehaviors/MortarAttack.cs
+++ Assets/Scripts/BossBehaviors/MortarAttack.cs
@@ -13,6 +13,9 @@ public class MortarAttack : MonoBehaviour
 	public float mortarMaxDistance = 20.0f;
 	public float delayBetweenMortars = 0.15f;
 
+	public bool usePredefinedTargetPos;
+	public Transform[] predefinedTargetPos;
+
 	private int _amountMortars;
 	private Timer _delayTimer;
 
@@ -51,7 +54,7 @@ public class MortarAttack : MonoBehaviour
 	{
 		GameObject mortarObject = Instantiate( mortar ) as GameObject;
 		Mortar m = mortarObject.GetComponent<Mortar>();
-		Vector3 targetPos = RandomTargetPosition();
+		Vector3 targetPos = GetTargetPosition();
 
 		m.Init( Random.Range( mortarSpeedMin, mortarSpeedMax ),
 				this.gameObject.transform.position,
@@ -63,12 +66,22 @@ public class MortarAttack : MonoBehaviour
 		_delayTimer.Reset( true );
 	}
 
-	private Vector3 RandomTargetPosition()
+	private Vector3 GetTargetPosition()
 	{
-		Vector3 pos = Random.insideUnitCircle * Random.Range( mortarMinDistance, mortarMaxDistance );
-		pos.z = pos.y; // Random.insideUnitCircle returns a 2D vector with (x, y), so we swap y with z for an accurate 3D position
-		pos += target.transform.position; // offset the psosition to the origin of the targeted object
-		pos.y = 0.0f;
+		Vector3 pos;
+
+		if ( !usePredefinedTargetPos )
+		{
+			pos = Random.insideUnitCircle * Random.Range( mortarMinDistance, mortarMaxDistance );
+			pos.z = pos.y; // Random.insideUnitCircle returns a 2D vector with (x, y), so we swap y with z for an accurate 3D position
+			pos += target.transform.position; // offset the psosition to the origin of the targeted object
+			pos.y = 0.0f;
+		}
+		else
+		{
+			pos = predefinedTargetPos[ Random.Range( 0, predefinedTargetPos.Length ) ].position;
+		}
+
 		return pos;
 	}
 }
