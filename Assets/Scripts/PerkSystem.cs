90ce7e0d63feb3aec45e5edac14ea2befa2bc66a
Westin Grube <westin28@hotmail.com>
2014-11-10 20:57:47 -0600

    #26 Pickup System
    DropSystem
    -Fixed a merge error

--- Assets/Scripts/PerkSystem.cs
+++ Assets/Scripts/PerkSystem.cs
@@ -114,16 +114,10 @@ public class PerkSystem : MonoBehaviour
 		//playerGun.amountOfMagazines += perk.magazinesMod;
 		//playerGun.reloadSpeed += perk.reloadMod;
 		//playerGun.infiniteAmmo = perk.infiniteAmmo || playerGun.infiniteAmmo;
-<<<<<<< HEAD
 		playerWeapons.SetBuffs( perk.fireRateMod, perk.damageMod, perk.reloadMod, perk.infiniteAmmo );
 		playerHealth.immune = perk.immunity || playerHealth.immune; //create shield or change healthbar if true
 		
 		if ( perk.gunDrop != null && playerWeapons.weapons.Count <= 3 )
-=======
-		playerHealth.immune = perk.immunity || playerHealth.immune;
-
-		if ( perk.gunDrop != null && playerWeapons.weapons.Count < 3 )
->>>>>>> origin/drop_system
 		{
 			if ( playerWeapons.weapons.Count == 3 )
 			{
