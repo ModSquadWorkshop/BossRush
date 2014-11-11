0943c61a68651fbc670ae069ab57dc75cdc5a968
Westin Grube <westin28@hotmail.com>
2014-11-10 22:19:36 -0600

    #26 #102 Minion Movement/Gun Minions
    MinionMovementTest
    -Randomized movement options with editable bounds/choices
    -Randomized minion gun cooldown

--- Assets/Scripts/BossBehaviors/Movement Scripts/Orbit.cs
+++ Assets/Scripts/BossBehaviors/Movement Scripts/Orbit.cs
@@ -8,14 +8,27 @@ public class Orbit : MonoBehaviour, ITargetBasedMovement
 	private Vector3 _distance;
 	private Vector3 _lookAt;
 
-	public float radius;
-	public float rotationSpeed;
-	public float travelSpeed;
+	//movement options
+	public float[] rotationSpeeds;
+	public float[] travelSpeeds;
+	public float radiusMin;
+	public float radiusMax;
+	public float cooldownMin;
+	public float cooldownMax;
+
+	private float _radius;
+	private float _rotationSpeed; //negative switches direction
+	private float _travelSpeed; //negative spirals towards target
+
 	public Gun minionGun;
+
 	// Use this for initialization
 	void Start () 
 	{
-
+		_radius = Random.Range( radiusMin, radiusMax );
+		minionGun.GetComponent<Weapon>().cooldown = Random.Range( cooldownMin, cooldownMax );
+		_rotationSpeed = rotationSpeeds[Random.Range( 0, rotationSpeeds.Length )];
+		_travelSpeed = travelSpeeds[Random.Range( 0, travelSpeeds.Length )];
 	}
 	
 	// Update is called once per frame
@@ -23,9 +36,9 @@ public class Orbit : MonoBehaviour, ITargetBasedMovement
 	{
 		if ( _target != null )
 		{
-			transform.RotateAround( _target.position, Vector3.up, rotationSpeed * Time.deltaTime );
-			_distance = (transform.position - _target.position).normalized * radius + _target.position;
-			transform.position = Vector3.MoveTowards( transform.position, _distance, Time.deltaTime * travelSpeed );
+			transform.RotateAround( _target.position, Vector3.up, _rotationSpeed * Time.deltaTime );
+			_distance = (transform.position - _target.position).normalized * _radius + _target.position;
+			transform.position = Vector3.MoveTowards( transform.position, _distance, Time.deltaTime * _travelSpeed );
 			minionGun.transform.rotation = Quaternion.LookRotation( _target.position - minionGun.transform.position );
 			if ( !minionGun.isOnCooldown )
 			{
