435b236b6c00dca1adb3431eadcf5db0548b0c64
Adam Graham <zigurous@gmail.com>
2014-11-13 19:20:14 -0600

    #94 initial setup and script creation for the spider tank burrowing/tunneling state.

---
+++ Assets/Scripts/BossBehaviors/SpiderTankStates/SpiderTankBurrowState.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderTankBurrowState : SpiderTankState 
+{
+	public Transform relocation;
+
+	public BurrowStateSettingsList burrowStateSettings;
+	private BurrowStateSettings[] _settings;
+
+	public override void OnEnable()
+	{
+		base.OnEnable();
+
+		_settings = new BurrowStateSettings[] { burrowStateSettings.phaseOneSettings, 
+												burrowStateSettings.phaseTwoSettings, 
+												burrowStateSettings.phaseThreeSettings, 
+												burrowStateSettings.phaseFourSettings };
+
+		DoBurrowAnimation();
+	}
+
+	void DoBurrowAnimation()
+	{
+		//.. do stuff
+		OnBurrowAnimationComplete(); // temp
+	}
+
+	void DoUnburrowAnimation()
+	{
+		//.. do stuff
+		OnUnburrowAnimationComplete(); // temp
+	}
+
+	void OnBurrowAnimationComplete()
+	{
+		// move the spider tank to its new position
+		spiderTank.transform.position = relocation.transform.position;
+
+		// start the unburrow sequence after x time underground
+		Invoke( "DoUnburrowAnimation", _settings[spiderTank.currentPhase].timeUnderground );
+	}
+
+	void OnUnburrowAnimationComplete()
+	{
+		//... do stuff, if necessary
+	}
+}
+
+
+[System.Serializable]
+public class BurrowStateSettings
+{
+	public float timeUnderground;
+}
+
+
+// this struct only exists to organize the settings in the inspector
+[System.Serializable]
+public class BurrowStateSettingsList
+{
+	public BurrowStateSettings phaseOneSettings;
+	public BurrowStateSettings phaseTwoSettings;
+	public BurrowStateSettings phaseThreeSettings;
+	public BurrowStateSettings phaseFourSettings;
+}
