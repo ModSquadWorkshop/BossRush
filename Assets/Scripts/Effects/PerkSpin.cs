eb986848cf31a1cd3b4d46bf2afe1530d1a70440
Westin Grube <westin28@hotmail.com>
2014-11-18 13:13:59 -0600

    #150 Perks Disappearing
    Indicators
    -Perks now destroyed after X seconds (lifetime variable on perkspin)

--- Assets/Scripts/Effects/PerkSpin.cs
+++ Assets/Scripts/Effects/PerkSpin.cs
@@ -5,16 +5,22 @@ public class PerkSpin : MonoBehaviour
 {
 	public Transform model;
 	public float spin;
-
+	public float lifetime;
 	private new Transform transform;
 
 	void Awake()
 	{
 		transform = GetComponent<Transform>();
+		Invoke( "End", lifetime );
 	}
 
 	void Update()
 	{
 		transform.Rotate( Vector3.up, spin * Time.deltaTime, Space.Self );
+	}
+
+	void End()
+	{
+		Destroy( this.gameObject );
 	}
 }
