d0ea3a23d2abc44582ceca21c372251b415ae2be
Westin Grube <westin28@hotmail.com>
2014-11-20 13:00:14 -0600

    BossHealth - Working health bar that displays only when boss is off screen

---
+++ Assets/Scripts/BossBehaviors/BossHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour 
+{
+	private HealthSystem _health;
+	private Scrollbar _scroll;
+	public Canvas healthbar;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		_health = GetComponentInParent<HealthSystem>();
+		_scroll = healthbar.GetComponentInChildren<Scrollbar>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( !this.renderer.isVisible )
+		{
+			healthbar.enabled = true;
+			_scroll.size = _health.percent;
+		}
+		else
+		{
+			healthbar.enabled = false;
+		}
+	}
+}
