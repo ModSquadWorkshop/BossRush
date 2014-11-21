3c39cac439ec32ecb33254559183055c52f26728
David LeGare <excaliburHisSheath@gmail.com>
2014-11-20 20:55:25 -0600

    Rocket Meshes - Adds the rocket mesh to the player's rocket launcher.

--- Assets/Scripts/Weapons/ExplodeOnDeath.cs
+++ Assets/Scripts/Weapons/ExplodeOnDeath.cs
@@ -9,13 +9,6 @@ public class ExplodeOnDeath : MonoBehaviour
 	public float explosionDamage;
 	public float explosionForce;
 
-	private DamageSystem _damageSystem;
-
-	void Start()
-	{
-		_damageSystem = GetComponent<DamageSystem>();
-	}
-
 	void Awake()
 	{
 		GetComponent<DeathSystem>().RegisterDeathCallback( Explode );
@@ -23,10 +16,12 @@ public class ExplodeOnDeath : MonoBehaviour
 
 	void Explode( GameObject obj )
 	{
+		DamageSystem damageSystem = GetComponent<DamageSystem>();
+
 		Collider[] collisions = Physics.OverlapSphere( transform.position, explosionRadius );
 		foreach ( Collider col in collisions )
 		{
-			if ( _damageSystem.IsTarget( col.tag ) )
+			if ( damageSystem.IsTarget( col.tag ) )
 			{
 				DealDamage( col.gameObject );
 
