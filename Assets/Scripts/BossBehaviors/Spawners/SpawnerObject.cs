0e38c06a8d78913bd9e79693c1267c3080bd5fb8
David LeGare <excaliburHisSheath@gmail.com>
2014-11-22 23:26:48 -0600

    Camera Pullback - The camera now zooms out when the boss or the player dies. - Maybe fixed a bug that was causing minions to be sunken into the ground.

--- Assets/Scripts/BossBehaviors/Spawners/SpawnerObject.cs
+++ Assets/Scripts/BossBehaviors/Spawners/SpawnerObject.cs
@@ -3,7 +3,5 @@ using System.Collections;
 
 public class SpawnerObject : MonoBehaviour 
 {
-	[HideInInspector] public SpawnPoint spawnPoint;
-
-	// more stuff here, if necessary
+	public SpawnPoint spawnPoint;
 }
