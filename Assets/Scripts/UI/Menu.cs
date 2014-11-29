00ca5384562913d75a7913639c133effc015501f
David LeGare <excaliburHisSheath@gmail.com>
2014-11-29 17:18:43 -0600

    Controller Menu Navigation - Added the ability to use the controller for the pause menu. - Still can't pause the game with the controller.

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+	public Button firstSelected;
+
+	void OnEnable()
+	{
+		GameObject.FindObjectOfType<EventSystem>().SetSelectedGameObject( firstSelected.gameObject );
+		firstSelected.Select();
+	}
+}
